9be863350fce44d8c9816c381eb1c1972534e2de
vishwa <vishwadeep.ghimire@gmail.com>
2023-09-01 06:46:07 -0500

    Project init

---
+++ src/main/java/edu/mum/cs/cs425/demowebapps/ELibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELibraryApplication.class, args);
+    }
+
+}
